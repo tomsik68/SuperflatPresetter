3af48ed16925971782df473964c0e66c8e6f065c
jasku <jasku@jasku-HP.(none)>
2013-04-17 14:19:50 +0200

    Finally managed to add some sources...

---
+++ dump_blockIDs.php
@@ -0,0 +1,41 @@
+<?php
+    $file = fopen("http://minecraft-ids.grahamedgecombe.com/", "r");
+    $line = "";
+    $id = "";
+    $name = "";
+    $table = FALSE;
+    while(!feof($file)){
+        $line = fgets($file);
+        if(stristr($line, '<table id="items">') != FALSE){
+            $table = TRUE;
+            continue;
+        }
+        if(stristr($line,"</table>") != FALSE){
+            $table = FALSE;
+            continue;
+        }
+        if(stristr($line,"<tr") != FALSE || stristr($line,"</tr>") != FALSE)
+            continue;
+        if($table == TRUE){
+            $line = str_replace('</td>', '', $line);
+            if(stristr($line,'<td class="id"') != FALSE){
+                $id = str_replace('<td class="id">', '', $line);
+                $id = str_replace(' ','',$id);  //remove whitespaces
+                $id = trim(preg_replace('/\s\s+/', ' ', $id));
+            }
+            if(stristr($line,'<td class="name">') != FALSE){
+                $line = str_replace('</a>', '', $line);
+                $name = "";
+                $i = strlen($line) - 1;
+                while($line[$i] != '>'){
+                    $name = $line[$i] . $name;
+                    --$i;
+                }
+                $name = trim(preg_replace('/\s\s+/', ' ', $name));
+                echo '["' .$id .'","' .$name .'"],'; 
+            }
+            
+        }
+    }
+    fclose($file);
+?>
---
+++ script.js
@@ -0,0 +1,45 @@
+/**
+ * @author Tomsik68
+ */
+function createBlockSelection() {
+	for (var i = 0; i < blocks.length; i++) {
+		document.write("<option value='" + blocks[i][0] + "'>" + blocks[i][1] + "</option>");
+	}
+}
+
+function createLayerSelections(count) {
+	for (var i = 0; i < count; i++) {
+		document.write('<tr><td>');
+		document.write('<select id="block_' + i + '">');
+		createBlockSelection();
+		document.write('</select>');
+		document.write('</td><td><input type="number" placeholder="Bottom" id="l' + i + '_bottom" /></td><td><input type="number" placeholder="Top" id="l' + i + '_top" /></td>');
+		document.write('</tr>');
+	}
+}
+
+function generate() {
+	
+	
+	var biome = document.getElementById("global_biome").value;
+	var villages = document.getElementById("villages").checked;
+	var villageSize = document.getElementById("villagesize").value;
+	var villageDist = document.getElementById("villagedistance").value;
+	var mineshafts = document.getElementById("mineshafts").checked;
+	var mineshaftChance = document.getElementById("mineshaftchance").value;
+	var strongholds = document.getElementById("strongholds").checked;
+	var strongholdDistance = document.getElementById("strongholddistance").value;
+	var strongholdCount = document.getElementById("strongholdcount").value;
+	var strongholdSpread = document.getElementById("strongholdspread").value;
+	var biomeFeatures = document.getElementById("biomefeatures").checked;
+	var dungeons = document.getElementById("dungeons").checked;
+	var decoration = document.getElementById("decoration").checked;
+	var waterLakes = document.getElementById("waterlakes").checked;
+	var lavaLakes = document.getElementById("lavalakes").checked;
+	//TODO parse layers
+	
+	document.getElementById("output").innerHTML = "Hello World";
+	alert("Don't press the button again, or the page will reset! Your code was generated!");
+	$("#output").slideToggle();
+	
+}
